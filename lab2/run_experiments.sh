b7a1228b17b24222a08e463fd6d14222be597d98
a13xbb <alex.blokh24@gmail.com>
2025-04-14 16:14:34 +0300

    finale grande commit

--- lab2/run_experiments.sh
+++ lab2/run_experiments.sh
@@ -1,26 +1,26 @@
 
-# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> three_nodes.txt
-# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> three_nodes.txt
-# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> three_nodes.txt
-# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> three_nodes.txt
-# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> three_nodes.txt
+docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> three_nodes.txt
+docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> three_nodes.txt
+docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> three_nodes.txt
+docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> three_nodes.txt
+docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> three_nodes.txt
 
 
-# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> three_nodes_opt.txt
-# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> three_nodes_opt.txt
-# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> three_nodes_opt.txt
-# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> three_nodes_opt.txt
-# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> three_nodes_opt.txt
+docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> three_nodes_opt.txt
+docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> three_nodes_opt.txt
+docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> three_nodes_opt.txt
+docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> three_nodes_opt.txt
+docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> three_nodes_opt.txt
 
-docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> one_node.txt
-docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> one_node.txt
-docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> one_node.txt
-docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> one_node.txt
-docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> one_node.txt
+# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> one_node.txt
+# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> one_node.txt
+# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> one_node.txt
+# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> one_node.txt
+# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv | tail -n 6 >> one_node.txt
 
 
-docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> one_node_opt.txt
-docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> one_node_opt.txt
-docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> one_node_opt.txt
-docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> one_node_opt.txt
-docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> one_node_opt.txt
+# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> one_node_opt.txt
+# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> one_node_opt.txt
+# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> one_node_opt.txt
+# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> one_node_opt.txt
+# docker exec -i spark-master spark-submit --master spark://spark-master:7077 spark_app.py -d hdfs://namenode:9000/fraud.csv -o | tail -n 6 >> one_node_opt.txt
